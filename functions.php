473d5d014f87961d17a30f0c36fba7cd50a04627
Mohamed Sawah <61028956+MohamedSawahZC@users.noreply.github.com>
2022-11-16 05:28:38 +0200

    Apply image upload to notes

--- functions.php
+++ functions.php
@@ -1,6 +1,34 @@
 <?php
 
+define('MB',1048576);
 
 function filter($payload){
     return htmlspecialchars(strip_tags($payload));
+}
+
+function ImageUpload($fieldName){
+    global $msgError ;
+    $imageName = rand(1000,10000) . $_FILES[$fieldName]["name"];
+    $imageTmp = $_FILES[$fieldName]["tmp_name"];
+    $imageSize = $_FILES[$fieldName]["size"];
+    $allowExt = array("png", "jpg", "jpeg", "gif","mp3","pdf");
+    $nameAsArray = explode(".",$imageName);
+    $ext = end($nameAsArray);
+    $ext = strtolower($ext);
+    if (!empty($imageName)&& !in_array($ext,$allowExt)){
+        $msgError[] = "Ext";
+    }
+    if ($imageSize>5 * MB){
+        $msgError[] = "Size";
+    }
+
+    if(empty($msgError)){
+        move_uploaded_file($imageTmp,"../Uploads/".$imageName);
+        return $imageName;
+    }else{
+        echo "<pre>";
+        print_r($msgError);
+        echo "<pre>";
+        return "Faild";
+    }
 }
--- notes/add.php
+++ notes/add.php
@@ -6,15 +6,19 @@ include "../connect.php";
 $title = filter($_POST['title']);
 $content = filter($_POST['content']);
 $user_id = filter($_POST['user_id']);
+$image = ImageUpload('image');
 
-$query = $connect->prepare("INSERT INTO `notes`( `title`, `content`,`user`) VALUES (? , ? , ?)");
+if ($image != 'Faild'){
+    $query = $connect->prepare("INSERT INTO `notes`( `title`, `content`,`user`,`image`) VALUES (? , ? , ?,?)");
 
-$query->execute(array($title,$content,$user_id));
+$query->execute(array($title,$content,$user_id,$image));
 
 $count = $query->rowCount();
-
 if ($count>0){
     echo json_encode(array("status" =>"Success"));
+}else{
+    echo json_encode(array("status" =>"Failure"));
+}
 }else{
     echo json_encode(array("status" =>"Failure"));
 }
